e119163abe312a6613291f68e1c7994acc3a555f
James Sully <sullyj3@gmail.com>
2019-11-17 15:04:48 +1100

    initial commit (not compiling)

---
+++ src/main.rs
@@ -0,0 +1,293 @@
+
+use std::fs::File;
+use std::io;
+use std::fmt;
+use std::io::Write;
+use std::io::prelude::*;
+use std::process::exit;
+
+use indoc::indoc;
+
+// #[macro_use]
+extern crate nom;
+
+use nom::{
+  IResult,
+  branch::alt,
+  bytes::complete::tag,
+  bytes::complete::tag_no_case,
+  bytes::complete::is_not,
+  character::complete::space1,
+  combinator::map,
+  multi::separated_nonempty_list,
+  number::complete::be_i32,
+  sequence::delimited,
+};
+
+#[derive(Debug)]
+enum ColType {
+    IntType,
+    StringType,
+}
+
+impl fmt::Display for ColType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,
+               "{}",
+               match self {
+                   ColType::IntType => "Int",
+                   ColType::StringType => "String",
+               });
+        Ok(())
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+enum Value {
+    IntType(i32),
+    StringType(String)
+}
+
+#[derive(Debug)]
+struct Column<'a> {
+    colname: &'a str,
+    coltype: ColType,
+}
+
+impl fmt::Display for Column<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}",
+               self.colname,
+               self.coltype
+        );
+        Ok(())
+    }
+}
+
+
+type Row = Vec<Value>;
+
+type TableSchema<'a> = Vec<Column<'a>>;
+
+fn temp_table() -> TableSchema<'static> {
+    vec![ Column { colname: "id",       coltype: ColType::IntType},
+          Column { colname: "username", coltype: ColType::StringType},
+          Column { colname: "email",    coltype: ColType::StringType} ]
+}
+
+#[derive(Debug, PartialEq)]
+struct RowParseError;
+
+// s will be a comma separated list of values
+fn parse_row(s: &str, schema: TableSchema) -> Result<Row, RowParseError> {
+    let vals: Vec<&str> = s.split(',').collect();
+    if vals.len() != schema.len() {
+        return Err(RowParseError);
+    }
+
+    let mut row = Vec::with_capacity(schema.len());
+
+    for (col, val_str) in schema.iter().zip(vals.iter()) {
+        let val = match col.coltype {
+            ColType::IntType => {
+                let i = val_str.parse::<i32>().map_err(|_| RowParseError)?;
+                Value::IntType(i)
+            }
+            ColType::StringType => Value::StringType(val_str.to_string())
+        };
+
+        row.push(val);
+    }
+
+    Ok( row )
+}
+
+fn parse_val(i: &str) -> IResult<&str, Value> {
+    alt((map(be_i32, |int: i32| Value::IntType(int)),
+         map(parse_string, |s: String| Value::StringType(s)) )
+       )(i)
+}
+
+fn parse_string(i: &str) -> IResult<&str, String> {
+    map(delimited(tag("\""),
+              is_not("\""),
+              tag("\"")
+    ), |s: &str| s.to_string() )(i)
+}
+
+fn parse_row2(row: &str) -> IResult<&str, Row> {
+    separated_nonempty_list(tag(","), parse_val)(row)
+}
+
+enum MetaCommandError {
+    UnrecognisedCommand,
+    CommandSyntaxError,
+}
+use crate::MetaCommandError::*;
+
+#[derive(Debug)]
+enum StatementPrepareError {
+    UnrecognisedStatement
+}
+//use StatementPrepareError::*;
+
+enum Statement {
+    Insert(Row),
+    Select
+}
+use Statement::*;
+
+fn show_schema(schema: &TableSchema) -> String {
+    let mut result = "| ".to_string();
+    for col in schema {
+        result += &col.to_string();
+        result += " | ";
+    }
+    result
+}
+
+fn main() -> std::io::Result<()> {
+    let schema = temp_table();
+
+    println!("Welcome to Vault!");
+    println!("\nUsing the following schema:\n{}", show_schema(&schema) );
+    shell(schema);
+
+    Ok(())
+}
+
+fn prompt(s: &str) {
+    print!("{}", s);
+    io::stdout().flush().unwrap();
+}
+
+fn shell(schema: TableSchema) {
+
+    prompt("vault> ");
+    for line in io::stdin().lock().lines().map( |l| l.unwrap() ) {
+
+        if line.trim().len() == 0 {
+            continue;
+        } else if line.starts_with(":") {
+            let mut chars = line.chars();
+            chars.next();
+            let command = chars.as_str();
+            match handle_meta_command(&command) {
+                Ok(()) => {}
+                Err(UnrecognisedCommand) => {
+                    println!("Unrecognised command: {}", line);
+                    usage();
+                }
+                Err(CommandSyntaxError) => {
+                    println!("invalid syntax: {}", line);
+                    usage();
+                }
+            }
+        } else {
+            match handle_statement(&line) {
+                Ok(()) => {}
+                Err(e) => println!("Error handling statement, {:?}", e),
+            }
+        }
+        prompt("vault> ");
+    }
+}
+
+fn handle_statement(stat: &str) -> Result<(), StatementPrepareError> {
+    unimplemented!();
+    //let statement = parse_statement(stat)?;
+    //execute_statement(statement);
+    //Ok(())
+}
+
+fn parse_insert(insert: &str) -> IResult<&str, Statement> {
+    let (i,_) = tag_no_case("INSERT")(insert)?;
+    let (i,_) = space1(i)?;
+    let (i,row) = delimited(tag("("), parse_row2, tag(")"))(i)?;
+
+    unimplemented!();
+}
+
+fn parse_statement(statement: &str) -> IResult<&str, Statement, StatementPrepareError> {
+    unimplemented!();
+    // let asdf = alt()
+
+    // let words: Vec<&str> = statement.split_whitespace().collect();
+    // let statement_type: &str = &words[0].to_uppercase();
+    // match statement_type {
+    //     "INSERT" => Ok(Insert),
+    //     "SELECT" => Ok(Select),
+    //     _        => Err(UnrecognisedStatement)
+    // }
+}
+
+fn execute_statement(s: Statement) {
+    match s {
+        Insert(_) => println!("insert not implemented"),
+        Select => println!("select not implemented"),
+    }
+}
+
+fn handle_meta_command(cmd: &str) -> Result<(), MetaCommandError> {
+    let words: Vec<&str> = cmd.split_whitespace().collect();
+    let words_slice: &[&str] = words.as_ref();
+
+    match words[0] {
+        "exit" => {
+            exit(0);
+        }
+        "help" => {
+            usage();
+        }
+        "open" => {
+            if let [_open, fname] = words_slice {
+                if let Ok(_file) = File::open(fname) {
+                    println!("{} exists!", fname);
+                } else {
+                    println!("{} does not exist.", fname);
+                }
+            } else {
+                return Err(CommandSyntaxError)
+            }
+        }
+        "create" => {
+            if let [_create, fname] = words_slice {
+                match File::create(fname) {
+                    Ok(_file) => println!("{} created!", fname),
+                    Err(e) => println!("creating {} failed. ({})", fname, e),
+                }
+            } else {
+                return Err(CommandSyntaxError)
+            }
+
+        }
+        _ => {
+            return Err(UnrecognisedCommand)
+        }
+    }
+    Ok(())
+}
+
+fn usage() {
+    let usage = indoc!("
+        Available commands:
+
+            :open FILENAME
+            :create FILENAME
+            :help
+            :exit
+        ");
+    println!("{}", usage)
+}
+
+#[test]
+fn test_parse_row_basic() {
+    let schema = temp_table();
+    let data = "0,james,sullyj3@gmail.com";
+    let result = parse_row(data, schema);
+    let expected = Ok(vec![Value::IntType(0),
+                           Value::StringType("james".to_string()),
+                           Value::StringType("sullyj3@gmail.com".to_string())] );
+    assert_eq!(result, expected);
+}
